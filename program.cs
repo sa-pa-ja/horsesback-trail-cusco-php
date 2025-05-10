d4da6cb5764659682d487c62d09e338f584f94e7
saturnino <161881@unsaac.edu.pe>
2025-05-10 10:52:45 -0500

    Keywords and reserved words

---
+++ program.cs
@@ -0,0 +1,7 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+Console.WriteLine("Hello my love lucy");
+
+int number = 400;
+string name = "Lucy";
+var favoriteAnimal = "Horsesback Trail Cusco";
